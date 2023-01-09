c3df6edfe8d8f10ebf563c4a44b7d7b5dc6a0807
shogidude <shogidude@protonmail.com>
2023-01-08 19:45:03 -0700

    Initial commit. It doesn't do anything yet.

---
+++ main.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+"""
+XYZgrid Editor
+
+Using TK to create a basic editor for generating
+XYZgrid maps for Evennia MUD projects.
+
+Author: ShogiDude
+Website: backrooms.net
+"""
+
+from tkinter import Tk, Canvas, Frame, BOTH, W
+
+current_map = r"""
++ 0 1 2 3
+
+3   #-#-#
+    |x|
+2 #-#-#
+    |  \
+1   #---#
+    |  /
+0 T-#-#
+
++ 0 1 2 3
+
+"""
+
+
+class XYZgridEditor(Frame):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.master.title("XYZgrid Editor")
+        self.pack(fill=BOTH, expand=1)
+
+        canvas = Canvas(self)
+
+        canvas.create_text(20, 20, anchor=W, font="Purisa", text="Currently selected character: -")
+
+        current_line_location = 30
+        map_lines = current_map.split('\n')
+        for line in map_lines:
+            canvas.create_text(20, current_line_location, anchor=W, font="Courier", text=line)
+            current_line_location += 15
+
+        canvas.pack(fill=BOTH, expand=1)
+
+
+def main():
+
+    root = Tk()
+    ex = XYZgridEditor()
+    root.geometry("800x640+300+300")
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
