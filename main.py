57646a53ee05a9e64397444fc77db7a6ed1e48c6
shogidude <shogidude@protonmail.com>
2023-01-31 19:08:31 -0700

    setting up the top and left frame

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Author: T Gene Davis (ShogiDude)
 Website: backrooms.net
 """
 
-from tkinter import Tk
+from tkinter import Tk, Frame, Label, TOP, X, Y, LEFT
 import ui.menus as me
 import map.mapping_frame as mf
 
@@ -24,6 +24,18 @@ def main():
 
     # Menubar for the app
     me.config_menubar(root)
+
+    # Top Frame
+    top_frame = Frame(root, background="blue")
+    top_label = Label(top_frame, text="Top Stuff")
+    top_label.pack()
+    top_frame.pack(expand=False, fill=X, side=TOP)
+
+    # Left Frame
+    side_frame = Frame(root, background="green")
+    side_label = Label(side_frame, text="Side Stuff")
+    side_label.pack()
+    side_frame.pack(expand=False, fill=Y, side=LEFT)
 
     # Map grid for drawing map
     mf.XYZgridFrame(root)
