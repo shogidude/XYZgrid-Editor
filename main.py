3d346316456451caafd7b6776d64effdefa75bc8
shogidude <shogidude@protonmail.com>
2023-02-02 09:54:21 -0700

    layout work

--- main.py
+++ main.py
@@ -27,14 +27,22 @@ def main():
 
     # Top Frame
     top_frame = Frame(root, background="blue")
-    top_label = Label(top_frame, text="Top Stuff")
-    top_label.pack()
+    top_label = Label(top_frame, text="Top Stuff 1")
+    top_label.pack(side=LEFT)
+    top_label = Label(top_frame, text="Top Stuff 2")
+    top_label.pack(side=LEFT)
+    top_label = Label(top_frame, text="Top Stuff 3")
+    top_label.pack(side=LEFT)
     top_frame.pack(expand=False, fill=X, side=TOP)
 
     # Left Frame
     side_frame = Frame(root, background="green")
-    side_label = Label(side_frame, text="Side Stuff")
-    side_label.pack()
+    side_label = Label(side_frame, text="Side Stuff 1")
+    side_label.pack(side=TOP)
+    side_label = Label(side_frame, text="Side Stuff 2")
+    side_label.pack(side=TOP)
+    side_label = Label(side_frame, text="Side Stuff 3")
+    side_label.pack(side=TOP)
     side_frame.pack(expand=False, fill=Y, side=LEFT)
 
     # Map grid for drawing map
