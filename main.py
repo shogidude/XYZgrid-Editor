f2c74c297136ca94f62b45e3c482ea1823b50f65
shogidude <shogidude@protonmail.com>
2023-02-07 17:09:42 -0700

    typing direction work

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Author: T Gene Davis (ShogiDude)
 Website: backrooms.net
 """
 
-from tkinter import Tk, Frame, Label, TOP, X, Y, LEFT
+from tkinter import Tk, Frame, Label, TOP, X, Y, LEFT, Radiobutton, StringVar, NW
 import ui.menus as me
 import map.mapping_frame as mf
 
@@ -44,6 +44,44 @@ def main():
     side_label = Label(side_frame, text="Side Stuff 3")
     side_label.pack(side=TOP)
     side_frame.pack(expand=False, fill=Y, side=LEFT)
+
+    direction_root = Frame(side_frame)
+
+    lbl = Label(direction_root, text="Type direction:   ")
+    lbl.pack(side=TOP)
+
+    direction_frame = Frame(direction_root)
+
+    selected = StringVar(direction_frame, 'E')
+
+    # top row
+    rnw = Radiobutton(direction_frame, text='', value='NW', var=selected)
+    rn = Radiobutton(direction_frame, text='', value='N', var=selected)
+    rne = Radiobutton(direction_frame, text='', value='NE', var=selected)
+
+    # middle row
+    rw = Radiobutton(direction_frame, text='', value='W', var=selected)
+    lc = Label(direction_frame, textvariable=selected)
+    re = Radiobutton(direction_frame, text='', value='E', var=selected)
+
+    # bottom row
+    rsw = Radiobutton(direction_frame, text='', value='SW', var=selected)
+    rs = Radiobutton(direction_frame, text='', value='S', var=selected)
+    rse = Radiobutton(direction_frame, text='', value='SE', var=selected)
+
+    lc.grid(column=1, row=1)
+    rnw.grid(column=0, row=0)
+    rn.grid(column=1, row=0)
+    rne.grid(column=2, row=0)
+    rw.grid(column=0, row=1)
+    re.grid(column=2, row=1)
+    rsw.grid(column=0, row=2)
+    rs.grid(column=1, row=2)
+    rse.grid(column=2, row=2)
+
+    direction_frame.pack()
+    direction_root.pack()
+
 
     # Map grid for drawing map
     mf.XYZgridFrame(root)
