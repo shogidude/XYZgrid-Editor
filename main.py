4cb3c4275c590747e2e3a23b807dd1e6dc94be60
shogidude <shogidude@protonmail.com>
2023-02-09 09:41:38 -0700

    updating UI to modern Tkinter

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ Author: T Gene Davis (ShogiDude)
 Website: backrooms.net
 """
 
-from tkinter import Tk, Frame, Label, TOP, X, Y, LEFT, Radiobutton, StringVar, NW
+from tkinter import Tk, TOP, X, Y, LEFT, StringVar
+from tkinter import ttk
 import ui.menus as me
 import map.mapping_frame as mf
 
@@ -26,48 +27,37 @@ def main():
     me.config_menubar(root)
 
     # Top Frame
-    top_frame = Frame(root, background="blue")
-    top_label = Label(top_frame, text="Top Stuff 1")
-    top_label.pack(side=LEFT)
-    top_label = Label(top_frame, text="Top Stuff 2")
-    top_label.pack(side=LEFT)
-    top_label = Label(top_frame, text="Top Stuff 3")
+    top_frame = ttk.Frame(root)
+    top_label = ttk.Label(top_frame, text="Top Stuff 1")
     top_label.pack(side=LEFT)
     top_frame.pack(expand=False, fill=X, side=TOP)
 
     # Left Frame
-    side_frame = Frame(root, background="green")
-    side_label = Label(side_frame, text="Side Stuff 1")
-    side_label.pack(side=TOP)
-    side_label = Label(side_frame, text="Side Stuff 2")
-    side_label.pack(side=TOP)
-    side_label = Label(side_frame, text="Side Stuff 3")
-    side_label.pack(side=TOP)
-    side_frame.pack(expand=False, fill=Y, side=LEFT)
+    side_frame = ttk.Frame(root)
 
-    direction_root = Frame(side_frame)
+    direction_root = ttk.Frame(side_frame)
 
-    lbl = Label(direction_root, text="Type direction:   ")
+    lbl = ttk.Label(direction_root, text="Type direction:   ")
     lbl.pack(side=TOP)
 
-    direction_frame = Frame(direction_root)
+    direction_frame = ttk.Frame(direction_root)
 
     selected = StringVar(direction_frame, 'E')
 
     # top row
-    rnw = Radiobutton(direction_frame, text='', value='NW', var=selected)
-    rn = Radiobutton(direction_frame, text='', value='N', var=selected)
-    rne = Radiobutton(direction_frame, text='', value='NE', var=selected)
+    rnw = ttk.Radiobutton(direction_frame, text='', value='NW', variable=selected)
+    rn = ttk.Radiobutton(direction_frame, text='', value='N', variable=selected)
+    rne = ttk.Radiobutton(direction_frame, text='', value='NE', variable=selected)
 
     # middle row
-    rw = Radiobutton(direction_frame, text='', value='W', var=selected)
-    lc = Label(direction_frame, textvariable=selected)
-    re = Radiobutton(direction_frame, text='', value='E', var=selected)
+    rw = ttk.Radiobutton(direction_frame, text='', value='W', variable=selected)
+    lc = ttk.Label(direction_frame, textvariable=selected, font="TkFixedFont")
+    re = ttk.Radiobutton(direction_frame, text='', value='E', variable=selected)
 
     # bottom row
-    rsw = Radiobutton(direction_frame, text='', value='SW', var=selected)
-    rs = Radiobutton(direction_frame, text='', value='S', var=selected)
-    rse = Radiobutton(direction_frame, text='', value='SE', var=selected)
+    rsw = ttk.Radiobutton(direction_frame, text='', value='SW', variable=selected)
+    rs = ttk.Radiobutton(direction_frame, text='', value='S', variable=selected)
+    rse = ttk.Radiobutton(direction_frame, text='', value='SE', variable=selected)
 
     lc.grid(column=1, row=1)
     rnw.grid(column=0, row=0)
@@ -81,6 +71,10 @@ def main():
 
     direction_frame.pack()
     direction_root.pack()
+
+
+    side_frame.pack(expand=False, fill=Y, side=LEFT)
+
 
 
     # Map grid for drawing map
